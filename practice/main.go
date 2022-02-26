17956889f8bf2708beb6bb51cc87398a8b1e7b38
Cassie Ang <cassie_ang@mymail.sutd.edu.sg>
2022-02-26 16:06:28 +0800

    commit for chapter 2.2

---
+++ practice/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+const aConst int =64
+
+func main() {
+
+	var aString string = "This is Go!"
+	fmt.Println(aString)
+	fmt.Printf("The variable's type is %T\n", aString)
+
+	var anInteger int = 42
+	fmt.Println(anInteger)
+
+	var defaultInt int
+	fmt.Println(defaultInt) // see that default int value is 0
+
+	var anotherString = "This is another string"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable's type is %T\n", anotherString)
+
+	var anotherInt = 53
+	fmt.Println(anotherInt)
+	fmt.Printf("The variable's type is %T\n", anotherInt)
+
+	myString := "This also a string"
+	fmt.Println(myString)
+	fmt.Printf("The variable's type is %T\n", myString)
+
+	// for const aConst
+	fmt.Println(aConst)
+	fmt.Printf("The variable's type is %T\n", aConst)
+
+	//fmt.Println("Hello from Go!")
+}
